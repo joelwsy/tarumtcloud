d6c0c31956891c25754c804e2df8afd3a8be94a4
Joelw <joelwsy-pm22@student.tarc.edu.my>
2024-05-12 21:56:15 +0800

    ad

---
+++ Template/_banner-area.php
@@ -0,0 +1,27 @@
+<!-- Owl-carousel -->
+<section id="banner-area">
+    <div class="owl-carousel owl-theme">
+        <div class="item fixed-height">
+            <img class="dimmed-image" src="./assets/Banner1.png" alt="Banner1">
+            <div class="banner-text">
+                <h1>Celebrate Graduation</h1>
+                <h4>Mark this important milestone with our special graduation bouquets. Handcrafted by floral artisans with vibrant blooms. Free same-day delivery in KL & Selangor.</h4>
+            </div>
+        </div>
+        <div class="item fixed-height">
+            <img class="dimmed-image" src="./assets/Banner2.png" alt="Banner2">
+            <div class="banner-text">
+                <h1>Graduation Gifts</h1>
+                <h4>Find the perfect gift for your graduate in our collection. From personalized items to memorable keepsakes. Free same-day delivery in KL & Selangor.</h4>
+            </div>
+        </div>
+        <div class="item fixed-height">
+            <img class="dimmed-image" src="./assets/Banner3.png" alt="Banner3">
+            <div class="banner-text">
+                <h1>Custom Graduation Bouquets</h1>
+                <h4>Create a unique bouquet for your graduate with our custom bouquet service. Choose from a variety of fresh flowers and design the perfect arrangement. Free same-day delivery in KL & Selangor.</h4>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- !Owl-carousel -->
